53a1927ac28328b9dfa047a44017b5f6da2d733d
Mehdi-YCH <mehdi.yc@kostango.com>
2025-03-23 00:14:24 +0100

    implementing Stack

--- algos/js/LinkedList.js
+++ algos/js/LinkedList.js
@@ -1,482 +1,482 @@
 class ListNode {
     constructor(value) {
-        this.value = value;
-        this.next = null;
+        this.value = value
+        this.next = null
     }
 }
 
 class LinkedList {
     constructor() {
-        this.head = null;
+        this.head = null
     }
 
     // Add a node to the end
     append(value) {
-        const node = new ListNode(value);
+        const node = new ListNode(value)
         if (!this.head) {
-            this.head = node;
-            return;
+            this.head = node
+            return
         }
-        let cur = this.head;
-        while (cur.next) cur = cur.next;
-        cur.next = node;
+        let cur = this.head
+        while (cur.next) cur = cur.next
+        cur.next = node
     }
 
     // Add a node to the beginning
     prepend(value) {
-        const node = new ListNode(value);
-        node.next = this.head;
-        this.head = node;
+        const node = new ListNode(value)
+        node.next = this.head
+        this.head = node
     }
 
     // Remove all nodes with the given value
     removeAll(value) {
-        if (!this.head) throw Error("bro the list is empty");
-        while (this.head && this.head.value == value) this.head = this.head.next;
-        let prev = null;
-        let cur = this.head;
+        if (!this.head) throw Error("bro the list is empty")
+        while (this.head && this.head.value == value) this.head = this.head.next
+        let prev = null
+        let cur = this.head
 
         while (cur) {
             if (value == cur.value) {
-                prev.next = cur.next;
-                cur = prev.next;
+                prev.next = cur.next
+                cur = prev.next
             } else {
-                prev = cur;
-                cur = cur.next;
+                prev = cur
+                cur = cur.next
             }
         }
     }
 
     // Remove the first node with the given value
     remove(value) {
-        if (!this.head) throw Error("bro the list is empty");
+        if (!this.head) throw Error("bro the list is empty")
         while (this.head && this.head.value == value) {
-            this.head = this.head.next;
-            return;
+            this.head = this.head.next
+            return
         }
-        let prev = null;
-        let cur = this.head;
+        let prev = null
+        let cur = this.head
 
         while (cur) {
             if (value == cur.value) {
-                prev.next = cur.next;
-                return;
+                prev.next = cur.next
+                return
             }
-            prev = cur;
-            cur = cur.next;
+            prev = cur
+            cur = cur.next
         }
     }
 
     // Get the size of the list
     size() {
-        let size = 0;
-        let cur = this.head;
+        let size = 0
+        let cur = this.head
         while (cur) {
-            size += 1;
-            cur = cur.next;
+            size += 1
+            cur = cur.next
         }
-        return size;
+        return size
     }
 
     // Get the sum of all node values
     sum() {
-        let rv_sum = 0;
-        let cur = this.head;
+        let rv_sum = 0
+        let cur = this.head
         while (cur) {
-            rv_sum += cur.value;
-            cur = cur.next;
+            rv_sum += cur.value
+            cur = cur.next
         }
-        return rv_sum;
+        return rv_sum
     }
 
     // Get the product of all node values
     mul() {
-        if (!this.head) return null;
-        let rv_mul = 1;
-        let cur = this.head;
+        if (!this.head) return null
+        let rv_mul = 1
+        let cur = this.head
         while (cur) {
-            rv_mul *= cur.value;
-            cur = cur.next;
+            rv_mul *= cur.value
+            cur = cur.next
         }
-        return rv_mul;
+        return rv_mul
     }
 
     // Check if the list is empty
     isEmpty() {
-        if (this.head) return false;
-        return true;
+        if (this.head) return false
+        return true
     }
 
     // Get the value of the first node
     getFirst() {
-        if (this.head) return this.head.value;
-        return null;
+        if (this.head) return this.head.value
+        return null
     }
 
     // Get the value of the last node
     getLast() {
-        if (!this.head) return null;
-        let cur = this.head;
-        let prev = null;
+        if (!this.head) return null
+        let cur = this.head
+        let prev = null
         while (cur) {
-            prev = cur;
-            cur = cur.next;
+            prev = cur
+            cur = cur.next
         }
-        return prev.value;
+        return prev.value
     }
 
     // Insert a node at a specific index
     insertAt(index, value) {
-        if (index < 0 || index > this.size()) throw Error("Invalid index");
+        if (index < 0 || index > this.size()) throw Error("Invalid index")
         if (index === 0) {
-            this.prepend(value);
-            return;
+            this.prepend(value)
+            return
         }
-        const node = new ListNode(value);
-        let cur = this.head;
+        const node = new ListNode(value)
+        let cur = this.head
         for (let i = 0; i < index - 1; i++) {
-            cur = cur.next;
+            cur = cur.next
         }
-        node.next = cur.next;
-        cur.next = node;
+        node.next = cur.next
+        cur.next = node
     }
 
     // Remove a node at a specific index
     removeAt(index) {
-        if (index < 0 || index >= this.size()) throw Error("Invalid index");
+        if (index < 0 || index >= this.size()) throw Error("Invalid index")
         if (index === 0) {
-            this.head = this.head.next;
-            return;
+            this.head = this.head.next
+            return
         }
-        let cur = this.head;
+        let cur = this.head
         for (let i = 0; i < index - 1; i++) {
-            cur = cur.next;
+            cur = cur.next
         }
-        cur.next = cur.next.next;
+        cur.next = cur.next.next
     }
 
     // Clear the list
     clear() {
-        this.head = null;
+        this.head = null
     }
 
     // Check if the list contains a value
     contains(value) {
-        if (!this.head) return false;
-        let cur = this.head;
+        if (!this.head) return false
+        let cur = this.head
         while (cur) {
-            if (cur.value == value) return true;
-            cur = cur.next;
+            if (cur.value == value) return true
+            cur = cur.next
         }
-        return false;
+        return false
     }
 
     // Get the index of the first occurrence of a value
     indexOf(value) {
-        let i = 0;
-        if (!this.head) return -1;
-        let cur = this.head;
+        let i = 0
+        if (!this.head) return -1
+        let cur = this.head
         while (cur) {
-            if (cur.value == value) return i;
-            cur = cur.next;
-            i += 1;
+            if (cur.value == value) return i
+            cur = cur.next
+            i += 1
         }
-        return -1;
+        return -1
     }
 
     // Get the index of the last occurrence of a value
     lastIndexOf(value) {
-        let i = 0;
-        let lastIndex = -1;
-        let cur = this.head;
+        let i = 0
+        let lastIndex = -1
+        let cur = this.head
         while (cur) {
-            if (cur.value == value) lastIndex = i;
-            cur = cur.next;
-            i += 1;
+            if (cur.value == value) lastIndex = i
+            cur = cur.next
+            i += 1
         }
-        return lastIndex;
+        return lastIndex
     }
 
     // Print the list as a string
     print() {
-        if (!this.head) console.log("empty");
-        let cur = this.head;
-        let rv = [];
+        if (!this.head) console.log("empty")
+        let cur = this.head
+        let rv = []
         while (cur) {
-            rv.push(cur.value);
-            cur = cur.next;
+            rv.push(cur.value)
+            cur = cur.next
         }
-        console.log(rv.join(" -> "));
+        console.log(rv.join(" -> "))
     }
 
     // Convert the list to a string
     toString() {
-        let cur = this.head;
-        let rv = [];
+        let cur = this.head
+        let rv = []
         while (cur) {
-            rv.push(cur.value);
-            cur = cur.next;
+            rv.push(cur.value)
+            cur = cur.next
         }
-        return rv.join(" -> ");
+        return rv.join(" -> ")
     }
 
     // Iterate over the list and apply a callback
     forEach(callback) {
-        let cur = this.head;
+        let cur = this.head
         while (cur) {
-            callback(cur.value);
-            cur = cur.next;
+            callback(cur.value)
+            cur = cur.next
         }
     }
 
     // Create a new list by applying a callback to each node
     map(callback) {
-        const newList = new LinkedList();
-        let cur = this.head;
+        const newList = new LinkedList()
+        let cur = this.head
         while (cur) {
-            newList.append(callback(cur.value));
-            cur = cur.next;
+            newList.append(callback(cur.value))
+            cur = cur.next
         }
-        return newList;
+        return newList
     }
 
     // Create a new list with nodes that satisfy the callback
     filter(callback) {
-        const newList = new LinkedList();
-        let cur = this.head;
+        const newList = new LinkedList()
+        let cur = this.head
         while (cur) {
             if (callback(cur.value)) {
-                newList.append(cur.value);
+                newList.append(cur.value)
             }
-            cur = cur.next;
+            cur = cur.next
         }
-        return newList;
+        return newList
     }
 
     // Reduce the list to a single value using a callback
     reduce(callback, initialValue) {
-        let accumulator = initialValue;
-        let cur = this.head;
+        let accumulator = initialValue
+        let cur = this.head
         while (cur) {
-            accumulator = callback(accumulator, cur.value);
-            cur = cur.next;
+            accumulator = callback(accumulator, cur.value)
+            cur = cur.next
         }
-        return accumulator;
+        return accumulator
     }
 
     // Merge another list into this one
     merge(list) {
         if (!this.head) {
-            this.head = list.head;
-            return this;
+            this.head = list.head
+            return this
         }
-        let cur = this.head;
+        let cur = this.head
         while (cur.next) {
-            cur = cur.next;
+            cur = cur.next
         }
-        cur.next = list.head;
-        return this;
+        cur.next = list.head
+        return this
     }
 
     // Sort the list using a comparator
     sort(comparator) {
-        const values = this.toArray();
-        values.sort(comparator);
-        this.clear();
+        const values = this.toArray()
+        values.sort(comparator)
+        this.clear()
         for (const value of values) {
-            this.append(value);
+            this.append(value)
         }
     }
 
     // Remove duplicate values from the list
     removeDuplicates() {
-        let cur = this.head;
+        let cur = this.head
         while (cur) {
-            let runner = cur;
+            let runner = cur
             while (runner.next) {
                 if (runner.next.value === cur.value) {
-                    runner.next = runner.next.next;
+                    runner.next = runner.next.next
                 } else {
-                    runner = runner.next;
+                    runner = runner.next
                 }
             }
-            cur = cur.next;
+            cur = cur.next
         }
     }
 
     // Swap two nodes in the list
     swap(node1, node2) {
-        if (node1 === node2) return;
+        if (node1 === node2) return
 
-        let prev1 = null, cur1 = this.head;
+        let prev1 = null, cur1 = this.head
         while (cur1 && cur1 !== node1) {
-            prev1 = cur1;
-            cur1 = cur1.next;
+            prev1 = cur1
+            cur1 = cur1.next
         }
 
-        let prev2 = null, cur2 = this.head;
+        let prev2 = null, cur2 = this.head
         while (cur2 && cur2 !== node2) {
-            prev2 = cur2;
-            cur2 = cur2.next;
+            prev2 = cur2
+            cur2 = cur2.next
         }
 
-        if (!cur1 || !cur2) return;
+        if (!cur1 || !cur2) return
 
-        if (prev1) prev1.next = cur2;
-        else this.head = cur2;
+        if (prev1) prev1.next = cur2
+        else this.head = cur2
 
-        if (prev2) prev2.next = cur1;
-        else this.head = cur1;
+        if (prev2) prev2.next = cur1
+        else this.head = cur1
 
-        const temp = cur1.next;
-        cur1.next = cur2.next;
-        cur2.next = temp;
+        const temp = cur1.next
+        cur1.next = cur2.next
+        cur2.next = temp
     }
 
     // Check if the list is a palindrome
     isPalindrome() {
-        const values = this.toArray();
-        return values.join("") === values.reverse().join("");
+        const values = this.toArray()
+        return values.join("") === values.reverse().join("")
     }
 
     // Detect if the list has a cycle
     detectCycle() {
-        let slow = this.head;
-        let fast = this.head;
+        let slow = this.head
+        let fast = this.head
         while (fast && fast.next) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if (fast === slow) return true;
+            slow = slow.next
+            fast = fast.next.next
+            if (fast === slow) return true
         }
-        return false;
+        return false
     }
 
     // Find the kth node from the end
     findKthFromEnd(k) {
-        let slow = this.head;
-        let fast = this.head;
+        let slow = this.head
+        let fast = this.head
         for (let i = 0; i < k; i++) {
-            if (!fast) return null;
-            fast = fast.next;
+            if (!fast) return null
+            fast = fast.next
         }
         while (fast) {
-            slow = slow.next;
-            fast = fast.next;
+            slow = slow.next
+            fast = fast.next
         }
-        return slow.value;
+        return slow.value
     }
 
     // Convert the list to a circular list
     toCircular() {
-        let cur = this.head;
+        let cur = this.head
         while (cur.next) {
-            cur = cur.next;
+            cur = cur.next
         }
-        cur.next = this.head;
+        cur.next = this.head
     }
 
     // Create a list from an array
     fromArray(array) {
         for (const value of array) {
-            this.append(value);
+            this.append(value)
         }
     }
 
     // Reverse the list
     reverse() {
-        let prev = null;
-        let cur = this.head;
+        let prev = null
+        let cur = this.head
         while (cur) {
-            const next = cur.next;
-            cur.next = prev;
-            prev = cur;
-            cur = next;
+            const next = cur.next
+            cur.next = prev
+            prev = cur
+            cur = next
         }
-        this.head = prev;
+        this.head = prev
     }
 
     // Convert the list to an array
     toArray() {
-        let cur = this.head;
-        let rv = [];
+        let cur = this.head
+        let rv = []
         while (cur) {
-            rv.push(cur.value);
-            cur = cur.next;
+            rv.push(cur.value)
+            cur = cur.next
         }
-        return rv;
+        return rv
     }
 
     // Get the next value in the list (iterator-like behavior)
     next() {
         if (!this.iterator) {
-            this.iterator = this.head;
+            this.iterator = this.head
         } else {
-            this.iterator = this.iterator.next;
+            this.iterator = this.iterator.next
         }
-        return this.iterator ? this.iterator.value : null;
+        return this.iterator ? this.iterator.value : null
     }
 }
 
 // Test cases using assert
 
 
-const list = new LinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
+const list = new LinkedList()
+list.append(1)
+list.append(2)
+list.append(3)
 
 // Test size()
-assert.strictEqual(list.size(), 3);
+assert.strictEqual(list.size(), 3)
 
 // Test sum()
-assert.strictEqual(list.sum(), 6);
+assert.strictEqual(list.sum(), 6)
 
 // Test mul()
-assert.strictEqual(list.mul(), 6);
+assert.strictEqual(list.mul(), 6)
 
 // Test isEmpty()
-assert.strictEqual(list.isEmpty(), false);
+assert.strictEqual(list.isEmpty(), false)
 
 // Test getFirst()
-assert.strictEqual(list.getFirst(), 1);
+assert.strictEqual(list.getFirst(), 1)
 
 // Test getLast()
-assert.strictEqual(list.getLast(), 3);
+assert.strictEqual(list.getLast(), 3)
 
 // Test contains()
-assert.strictEqual(list.contains(2), true);
-assert.strictEqual(list.contains(4), false);
+assert.strictEqual(list.contains(2), true)
+assert.strictEqual(list.contains(4), false)
 
 // Test indexOf()
-assert.strictEqual(list.indexOf(3), 2);
+assert.strictEqual(list.indexOf(3), 2)
 
 // Test lastIndexOf()
-list.append(2);
-assert.strictEqual(list.lastIndexOf(2), 3);
+list.append(2)
+assert.strictEqual(list.lastIndexOf(2), 3)
 
 // Test remove()
-list.remove(2);
-assert.strictEqual(list.contains(2), true); // Only one 2 was removed
+list.remove(2)
+assert.strictEqual(list.contains(2), true) // Only one 2 was removed
 
 // Test removeAll()
-list.removeAll(2);
-assert.strictEqual(list.contains(2), false);
+list.removeAll(2)
+assert.strictEqual(list.contains(2), false)
 
 // Test clear()
-list.clear();
-assert.strictEqual(list.size(), 0);
+list.clear()
+assert.strictEqual(list.size(), 0)
 
 // Test fromArray()
-list.fromArray([4, 5, 6]);
-assert.strictEqual(list.size(), 3);
+list.fromArray([4, 5, 6])
+assert.strictEqual(list.size(), 3)
 
 // Test reverse()
-list.reverse();
-assert.deepStrictEqual(list.toArray(), [6, 5, 4]);
+list.reverse()
+assert.deepStrictEqual(list.toArray(), [6, 5, 4])
 
 // Test detectCycle()
-assert.strictEqual(list.detectCycle(), false);
-list.toCircular();
-assert.strictEqual(list.detectCycle(), true);
+assert.strictEqual(list.detectCycle(), false)
+list.toCircular()
+assert.strictEqual(list.detectCycle(), true)
 
-console.log("All tests passed!");
+console.log("All tests passed!")
--- algos/js/Stack.js
+++ algos/js/Stack.js
@@ -1,41 +1,100 @@
+const assert = require("assert")
+class ListNode {
+    constructor(value) {
+        this.value = value
+        this.next = null
+    }
+}
+
+
 class Stack {
     constructor() {
-        this.items = [];
+        this.head = null
     }
 
-    // TODO: Push an element onto the stack
-    push(element) {
-        // Implement this
-    }
 
-    // TODO: Pop an element from the stack
+    // TODO: Push an value onto the stack
+    push(value) {
+        const node = new ListNode(value)
+        if (!this.head) {
+            this.head = node
+            return
+        }
+        let cur = this.head
+        while (cur.next) cur = cur.next
+        cur.next = node
+    }
+    
+
+    // TODO: Pop an value from the stack
     pop() {
-        // Implement this
+        if (!this.head) return null
+        let prev = null
+        let cur = this.head
+        while (cur.next){ 
+            prev = cur
+            cur = cur.next
+        }
+        const rv = cur.value
+        prev.next = null
+        return rv
     }
 
-    // TODO: Peek at the top element without removing it
+    // TODO: Peek at the top value without removing it
     peek() {
-        // Implement this
+        if (!this.head) return null
+        let prev = null
+        let cur = this.head
+        while (cur.next){ 
+            prev = cur
+            cur = cur.next
+        }
+        return cur.value
     }
 
     // TODO: Check if the stack is empty
     isEmpty() {
-        // Implement this
+        if (!this.head) return true
+        return false
     }
 
     // TODO: Get the size of the stack
     size() {
-        // Implement this
+        let size = 0
+        let cur = this.head
+        while (cur) {
+            size += 1
+            cur = cur.next
+        }
+        return size
     }
+
+
+    print() {
+        if (!this.head) console.log("empty")
+        let cur = this.head
+        let rv = []
+        while (cur) {
+            rv.push(cur.value)
+            cur = cur.next
+        }
+        console.log(rv.join(" -> "))
+    }
+
 }
 
 // Test cases
-const stack = new Stack();
-stack.push(1);
-stack.push(2);
-stack.push(3);
+const stack = new Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
 
-assert.strictEqual(stack.pop(), 3, "Test 1 Failed");
-assert.strictEqual(stack.peek(), 2, "Test 2 Failed");
-assert.strictEqual(stack.size(), 2, "Test 3 Failed");
-assert.strictEqual(stack.isEmpty(), false, "Test 4 Failed");
+
+stack.print()
+assert.strictEqual(stack.pop(), 3, "Test 1 Failed")
+assert.strictEqual(stack.peek(), 2, "Test 2 Failed")
+assert.strictEqual(stack.size(), 2, "Test 3 Failed")
+assert.strictEqual(stack.isEmpty(), false, "Test 4 Failed")
+
+console.log('\x1b[92m%s\x1b[0m', ' * All tests passed');
+
